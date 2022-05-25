bea52630c39b9648f7e1b54806e51470d238b52e
hsw0715 <hsw0715@naver.com>
2022-05-25 10:02:09 +0900

    Design: 로그인 화면 변경

--- ssafybottle_manager/app/src/main/java/com/ssafy/ssafybottle_manager/utils/view/ImageUtil.kt
+++ ssafybottle_manager/app/src/main/java/com/ssafy/ssafybottle_manager/utils/view/ImageUtil.kt
@@ -3,4 +3,4 @@ package com.ssafy.ssafybottle_manager.utils.view
 import android.view.View
 
 fun View.getResourceId(url: String?): Int =
-    resources.getIdentifier("@drawable/${url!!.split(".")[0]}", "drawable", "com.ssafy.smartstore")
+    resources.getIdentifier("@drawable/${url!!.split(".")[0]}", "drawable", "com.ssafy.ssafybottle_manager")
